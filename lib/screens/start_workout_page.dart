6156cc18b19e60ad50f20b569c5c057939f19494
Vitalii Mordak <vitalii_mordak@epam.com>
2023-04-22 20:22:03 +0300

    Specify current date for start_workout_page.dart in Format 'Day of Week, Date Month Year'

--- lib/screens/start_workout_page.dart
+++ lib/screens/start_workout_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gym_tracker/screens/current_workout_page.dart';
+import 'package:intl/intl.dart';
 
 class StartWorkoutPage extends StatefulWidget {
   const StartWorkoutPage({Key? key}) : super(key: key);
@@ -11,13 +13,31 @@ class StartWorkoutPage extends StatefulWidget {
 class _StartWorkoutPageState extends State<StartWorkoutPage> {
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('EEEEE, d MMMM yyyy').format(now);
     return Scaffold(
-      appBar: AppBar(title: Text('Start Workout')),
+      appBar: AppBar(
+        title: Text('Start Workout'),
+        foregroundColor: Colors.black87,
+        backgroundColor: CupertinoColors.systemBackground,
+        elevation: 0.8,
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Today\'s date in format like Sun, 19 Feb 2023'),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              formattedDate,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade700,
+                  fontSize: 15),
+            ),
+          ),
+          const Expanded(child: SizedBox()),
           Padding(
             padding: const EdgeInsets.all(15.0),
             child: ElevatedButton(
@@ -29,7 +49,8 @@ class _StartWorkoutPageState extends State<StartWorkoutPage> {
                         builder: (context) => CurrentWorkoutPage()));
               },
             ),
-          )
+          ),
+          const Expanded(child: SizedBox()),
         ],
       ),
     );
