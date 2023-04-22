f59e113793ce55d30dc08566b9e6bd9705e4d042
Vitalii Mordak <vitalii_mordak@epam.com>
2023-04-22 20:21:09 +0300

    Add calendar widget for history_page.dart

--- lib/screens/history_page.dart
+++ lib/screens/history_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({Key? key}) : super(key: key);
@@ -8,11 +9,50 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+
   @override
   Widget build(BuildContext context) {
-    return const Icon(
-      Icons.access_time_rounded,
-      size: 150,
+    return Scaffold(
+      appBar: AppBar(title: Text('History')),
+      body: Column(
+        children: [
+          //todo: add elevation to table calendar
+          //todo: add bottom small line to display widget is can be swiped
+          //todo: change style of AppBar and Calendar
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2060, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            availableCalendarFormats: {
+
+              CalendarFormat.month: 'Month',
+              CalendarFormat.week: 'Week'
+            },
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+            ),
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _focusedDay = focusedDay;
+                _selectedDay = selectedDay;
+              });
+            },
+          )
+        ],
+      ),
     );
   }
 }
